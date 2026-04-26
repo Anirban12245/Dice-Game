b8adba2dcd6166d80390cb2f8ed3de0a2b1c3460
Anirban12245 <anirbanghosh053@gmail.com>
2026-04-26 16:19:02 +0530

    Initial commit - Dice Game

---
+++ dicegame.py
@@ -0,0 +1,9 @@
+import random
+
+while True:
+    input("Press enter to roll the dice: ")
+    roll = random.randint(1, 6)
+    print("Dice rolled", roll)
+    choice = input("Roll again? (y/n): ").lower()
+    if choice=="n":
+        break
